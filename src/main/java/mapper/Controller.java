274dd2e7e5eb03e3ce69d25470cdabf25a6ef5dc
wenjian80 <33510286+wenjian80@users.noreply.github.com>
2019-10-28 12:23:21 +0800

    Update codes
    Update by BJ

--- src/main/java/mapper/Controller.java
+++ src/main/java/mapper/Controller.java
@@ -1,5 +1,6 @@
 package mapper;
 
+
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
