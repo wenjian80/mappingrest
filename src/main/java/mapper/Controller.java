72d7835903fda9c625849f4c4a539ea26cedc3ba
wenjian80 <33510286+wenjian80@users.noreply.github.com>
2019-10-31 21:38:20 +0800

    A
    A

--- src/main/java/mapper/Controller.java
+++ src/main/java/mapper/Controller.java
@@ -1,6 +1,5 @@
 package mapper;
 
-
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
